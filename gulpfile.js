b12675fd02cced98fcd0d873f69347ee9e620d34
Damian Escobar <damianescobar2001@gmail.com>
2024-11-20 17:47:45 -0300

    Enpezando JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
-import { src, dest, watch } from "gulp";
+import { src, dest, watch, series } from "gulp";
 
 const sass = gulpSass(dartSass);
 
+export function js (done)  {
+  src('src/js/app.js')
+    .pipe( dest('build/js'))
+
+    done();
+}
+
 export function css(done) {
-  src('src/scss/app.scss') // Archivo de entrada
+  src('src/scss/app.scss', {sourcemaps: true}) // Archivo de entrada
     .pipe(sass().on('error', sass.logError)) // Manejo de errores
-    .pipe(dest('build/css')); // Carpeta de salida
+    .pipe(dest('build/css', {sourcemaps: true})); // Carpeta de salida
 
   done();
 }
 
 export function dev() {
   watch('src/scss/**/*.scss', css)
+  watch('src/js/**/*.js', js)
 }
+
+export default series(js,css,dev);
